febb1bc14b0316d3cc5e5d44735a15cd9a6e926e
kirill <kirill.dorogan.89@gmail.com>
2018-11-14 05:04:56 +0200

    FR: get product by barcode

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use App\Controller\Common\FormErrorHandlerTrait;
+use App\Entity\Product\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Swagger\Annotations as SWG;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class ProductController
+{
+    use FormErrorHandlerTrait;
+
+    /**
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Product",
+     *     @SWG\Schema(
+     *        type="object",
+     *        @SWG\Property(property="id", type="string", example="1234567"),
+     *        @SWG\Property(property="barcode", type="string", example="1234567"),
+     *        @SWG\Property(property="name", type="string", example="product1"),
+     *        @SWG\Property(property="cost", type="float", enum={9.99}),
+     *        @SWG\Property(
+     *             property="vat_class_type",
+     *             type="integer",
+     *             example="1",
+     *             description="1 - 6%, 2 - 21%"),
+     *    ))
+     * )
+     * @SWG\Response(
+     *        response=404,
+     *        description="Not found",
+     *        @SWG\Schema(
+     *            type="object",
+     *            @SWG\Property(property="code", type="integer", enum={404}),
+     *            @SWG\Property(property="message", type="string", example="Not found")
+     *        )
+     * )
+     * @SWG\Tag(name="Product")
+     *
+     * @Route("/api/product/{barcode}", name="get_product", methods="GET")
+     * @return  JsonResponse
+     */
+    public function createProduct(
+        string $barcode,
+        SerializerInterface $serializer,
+        EntityManagerInterface $entityManager
+    ) {
+        $product = $entityManager->getRepository(Product::class)->findBy(['barcode' => $barcode]);
+        if (!$product) {
+            return new JsonResponse('Not found', Response::HTTP_NOT_FOUND);
+        }
+
+        return new JsonResponse(
+            $serializer->serialize($product, 'json', ['groups' => 'full']),
+            Response::HTTP_CREATED
+        );
+    }
+}
